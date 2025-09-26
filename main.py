7f17fa18b00490abc93fd8c2f522d80a99554f98
George v s <george@superion.in>
2025-09-26 14:28:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from tools import doc_endpoint
 app = FastAPI()
 
 
-app.include_router(pdf_endpoint.router, prefix="/api/pdf", tags=["PDF"])
-app.include_router(presentation_endpoint.router, prefix="/api/ppt", tags=["PPT"])
-app.include_router(doc_endpoint.router,prefix="/api/docs",tags=["DOCX"])
+app.include_router(pdf_endpoint.router, prefix="/api/utility-service/pdf", tags=["PDF"])
+app.include_router(presentation_endpoint.router, prefix="/api/utility-service/ppt", tags=["PPT"])
+app.include_router(doc_endpoint.router,prefix="/api/utility-service/docs",tags=["DOCX"])
 
 
