b14377b13f85240378049109a9e2b53a0822443d
George v s <george@superion.in>
2025-09-26 14:24:49 +0530

    Feat: DockerFile Added
    ..

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from tools import pdf_generator
-from tools import ppt
-from tools import doc_gen
+from tools import pdf_endpoint
+from tools import presentation_endpoint
+from tools import doc_endpoint
 app = FastAPI()
 
 
-app.include_router(pdf_generator.router, prefix="/api/pdf", tags=["PDF"])
-app.include_router(ppt.router, prefix="/api/ppt", tags=["PPT"])
-app.include_router(doc_gen.router,prefix="/api/docs",tags=["DOCX"])
+app.include_router(pdf_endpoint.router, prefix="/api/pdf", tags=["PDF"])
+app.include_router(presentation_endpoint.router, prefix="/api/ppt", tags=["PPT"])
+app.include_router(doc_endpoint.router,prefix="/api/docs",tags=["DOCX"])
 
 
